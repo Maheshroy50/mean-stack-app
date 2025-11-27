91294ea2018569be4c4ea22ae51ad2a315f23332
Mahesh Bhoopathi Rao <47824626+Maheshroy50@users.noreply.github.com>
2025-11-27 22:59:16 +0530

    Delete Documents/DevOps learning /Internship assign directory

--- Documents/DevOps learning /Internship assign/terraform/keypair.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_key_pair" "auth" {
-  key_name   = var.key_name
-  public_key = file("mean-stack-key.pub")
-}
--- Documents/DevOps learning /Internship assign/terraform/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "instance_public_ip" {
-  description = "Public IP address of the EC2 instance"
-  value       = aws_instance.app_server.public_ip
-}
-
-output "ssh_command" {
-  description = "Command to SSH into the instance"
-  value       = "ssh -i ${var.key_name}.pem ubuntu@${aws_instance.app_server.public_ip}"
-}
